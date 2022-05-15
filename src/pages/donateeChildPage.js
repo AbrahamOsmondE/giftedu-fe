cddf56adab8a5e3113cbbc3c497ed6db75035f56
Ferlita Halim <ferlitahalim@gmail.com>
2022-05-16 01:50:26 +0800

    fix donateeChildPage

--- src/pages/donateeChildPage.js
+++ src/pages/donateeChildPage.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "components";
-import Button from "components/button";
 import apiProvider from "../utils/apiProvider";
-import authProvider from "../utils/authProvider";
+import { useParams } from "react-router-dom";
 
 let headerLink = [
   {
@@ -12,58 +11,89 @@ let headerLink = [
 ];
 
 export default function DonateeChildPage() {
-  const [details, setDetials] = useState({});
+  const params = useParams();
+  const child_id = params.id;
+
+  const [details, setDetials] = useState();
+
+  useEffect(() => {
+      apiProvider.getChild(child_id)
+      .then(res => {
+          setDetials(res.data)
+      })
+  }, [child_id])
+
+
+  if (!details) {
+    return (
+      <div>
+        <Header listOfLink={headerLink} />
+        Loading...
+      </div>
+    )
+  }
+
   return (
     <div>
       <Header listOfLink={headerLink} />
-      <div class="max-w-sm w-full lg:max-w-full lg:flex">
-        <div
-          class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-          style={{ backgroundImage: details?.photo }}
-          title="Woman holding a mug"
-        ></div>
-        <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-          <div class="flex justify-between items-center mb-4 pr-64">
-            <div class="text-sm">
-              <p class="text-gray-900 leading-none text-lg">{details?.name}</p>
-              <p class="text-gray-600 text-lg">{details?.donee}</p>
+      <div className="px-20 grid grid-cols-2 gap-4 pb-10">
+        <div className="min-h-full flex items-center justify-center py-4">
+          <div className="h-40 w-40 overflow-hidden rounded-lg">
+              <img className="object-fill" src={details.photo ? details.photo : "https://i.imgflip.com/1slnr0.jpg"} alt={details.name}/>
+          </div>
+        </div>
+        <div className="border border-gray-400 bg-white rounded-b p-4 flex rounded-lg flex-col justify-between leading-normal">
+          <div className="flex justify-between items-center mb-4 pr-5">
+            <div className="text-sm">
+              <p className="text-gray-900 leading-none text-lg">{details.name}</p>
+              <p className="text-gray-600 text-lg">{details.donee}</p>
             </div>
             <div>
-              <ul class="w-1.5">
+              <ul className="w-1.5">
                 <li className="mr-3">
-                  <div className="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4 rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-100 bg-indigo-700">
+                  <button className="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4 rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-100 bg-indigo-700">
                     Subscribe
-                  </div>
+                  </button>
                 </li>
               </ul>
-              <p class="text-lg">{details?.subscription_cost}month</p>
+              <p className="text-lg text-right pr-8">$ {details.subscription_cost} /month</p>
             </div>
           </div>
-          <div class="mb-8">
-            <div class="text-gray-900 font-bold text-xl mb-2">
-              Current Donation per Month:{details?.fund}
+          <div className="mb-8">
+            <div className="text-gray-900 font-bold text-xl mb-2">
+              Current Donation per Month: ${details.fund}
             </div>
-            <p class="text-gray-700 text-base">{details?.description}</p>
+            <p className="text-gray-700 text-base">{details?.description}</p>
           </div>
         </div>
       </div>
-      <div class="max-w-6xl flex ml-48 justify-between font-bold text-xl my-3 ">
-        <div class="text-xl">Updates</div>
-        <div class="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4 rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-100 bg-indigo-700">
-          + Add Updates
-        </div>
+
+      <div className="max-w-6xl flex ml-48 flex-wrap font-bold text-xl my-3 ">
+        Updates
+        <button
+            className="group relative w-auto flex justify-center py-1 pl-2 pr-4 border border-transparent text-sm font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-5"
+            onClick={()=>{window.location.href = `/donatee/child/${child_id}/update`}}
+        >
+            + Add Post
+        </button>
       </div>
-      {details?.posts?.map((update) => {
-        const date = new Date(update?.created);
+      {details.posts.length === 0 && 
+        <div className="max-w-6xl flex ml-48 flex-wrap text-l my-3 ">
+          No update yet ... 
+        </div>
+      }
+      {details.posts.length > 0 && 
+      details.posts.map((update) => {
+        const date = (new Date(update.created)).toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
         return (
-          <div class="max-w-6xl flex ml-48 flex-wrap">
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-              <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">{update?.title}</div>
-                <p class="text-gray-700 text-base">{update?.text}</p>
+          <div className="max-w-6xl flex ml-48 flex-wrap" key={update.id}>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{update?.title}</div>
+                <p className="text-gray-700 text-base">{update?.text}</p>
               </div>
-              <div class="px-6 pt-4 pb-2 flex flex-row-reverse">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              <div className="px-6 pt-4 pb-2 flex flex-row-reverse">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                   {date}
                 </span>
               </div>
